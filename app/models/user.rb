b5a29b455bd8ce0b21de25714b96a6a2616eaa65
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-03-29 21:14:58 +0300

    added user model

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+    validates_uniqueness_of :username, uniqueness: { case_sensitive: false}
+    validates :username, :password,  :name, presence: true
+  
+    has_secure_password
+  
+    scope :customers, -> { where(type: 'Customer')}
+    scope :sellers, -> { where(type: 'Seller')}
+  
+  
+  
+  # def invalidate_token
+  #   self.update_columns(auth_token: nil)
+  #
+  # end
+  #
+  # def self.validate_login(username, password)
+  #   user = find_by(username: username)
+  #   if user && user.authenticate(password)
+  #     user
+  #   end
+  # end
+  
+  end
+  
