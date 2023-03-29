fddadf8908dc71f6dde3d4fe0d276f0296eee13b
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-03-29 21:13:21 +0300

    added products model

---
+++ app/models/product.rb
@@ -0,0 +1,8 @@
+class Product < ApplicationRecord
+    # belongs_to :seller
+    # has_many :carts
+    # has_many :customers, through :carts
+    has_many :sellers, class_name: "Seller"
+    has_many :customers, class_name: "Customer"
+  end
+  
